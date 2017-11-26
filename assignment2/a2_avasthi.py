b8438fa18f0c6d81ee81ec31a5d8a97c1d64f739
prakhar.avasthi <prakhar.avasthi@stonybrook.edu>
2017-11-26 15:53:52 -0500

    Q3c

--- assignment2/a2_avasthi.py
+++ assignment2/a2_avasthi.py
@@ -1,5 +1,7 @@
 from pyspark import SparkContext
 from tifffile import TiffFile
+from numpy.linalg import svd
+
 import re
 import os
 import io
@@ -79,13 +81,14 @@ def matrixDiff(fileName, reducedArr):			#Q2c, Q2d and Q2e
 			feature[i] = 1
 		else:
 			feature[i] = 0
-	if(fileName == '3677454_2025195.zip-1' or fileName == '3677454_2025195.zip-18'):
-		print(feature)
+	#if(fileName == '3677454_2025195.zip-1' or fileName == '3677454_2025195.zip-18'):
+		#print(feature)
 	return (fileName, feature)
 
 def printfeature(name, arr):				#Q2f
 	if(name == '3677454_2025195.zip-1' or name == '3677454_2025195.zip-18'):
 		print(arr)
+	return (name, arr)
 
 def sign(fileName, feature):				#Q3a			
 	bytes = np.zeros(shape=(128))
@@ -127,24 +130,43 @@ def printSimiliar(fileName, similiar_files):
 		i = 0
 		simi = []
 		for file in similiar_files:
-			if file.strip() != '3677454_2025195.zip-1':
+			if file.strip() != '3677454_2025195.zip-1' and file.strip() != '':
 				simi.append(file.strip())
 				i += 1
 			if(i == 20):
 				break
-		print(fileName, simi)
+		return (fileName, simi)
 	elif(fileName == '3677454_2025195.zip-18'):
 		i = 0
 		simi = []
 		for file in similiar_files:
-			if file.strip() != '3677454_2025195.zip-18':
+			if file.strip() != '3677454_2025195.zip-18' and file.strip() != '':
 				simi.append(file.strip())
 				i += 1
 			if(i == 20):
 				break
-		print(fileName, simi)
+		return (fileName, simi)
 
-	return (fileName, )
+def dimenReduce(inp):
+	from scipy import linalg
+	fileName = []
+	feature = []
+	std_array = []
+	result = []
+	for part in inp:
+		fileName.append(part[0])
+		feature.append(part[1])
+
+	for i in range(len(feature)):
+		mu, std = np.mean(feature[i], axis=0), np.std(feature[i], axis=0)
+		img_diffs_zs = (feature[i] - mu) / std
+		std_array.append(img_diffs_zs)
+	
+	U, s, Vh = linalg.svd(np.array(std_array), full_matrices=1)
+
+	for j in range(len(U)):
+		result.append((fileName[j], U[j:j+1, 0:10].flatten()))
+	return result
 			
 
 if __name__ == "__main__":
@@ -166,22 +188,64 @@ if __name__ == "__main__":
 	file_image_array = filesRdd.map(lambda a: (a[0][a[0].rfind("/")+1:], getOrthoTif(a[1])))
 	blocks = file_image_array.flatMap(lambda a:divideImage(a[0], a[1]))		#Q1d
 	print_blocks = blocks.map(lambda a:printrgb(a[0], a[1]))			#Q1e
-	#print_blocks.collect()
+	print_blocks.persist()
 
-	intensity = print_blocks.map(lambda a:calculateIntensity(a[0], a[1]))			#Q2a
+	intensity = print_blocks.map(lambda a:calculateIntensity(a[0], a[1]))		#Q2a
 	reduced = intensity.map(lambda a:reduceFactor(a[0], a[1]))			#Q2b
 	major = reduced.map(lambda a:matrixDiff(a[0], a[1]))				#Q2c, Q2d and Q2e
-	#print_feature = major.map(lambda a:printfeature(a[0], a[1]))			#Q2f
-	#print_feature.collect()
+	print_feature = major.map(lambda a:printfeature(a[0], a[1]))			#Q2f
+	print_feature.persist()
 
-	signatures = major.map(lambda a:sign(a[0], a[1]))				#Q3a
+	signatures = print_feature.map(lambda a:sign(a[0], a[1]))			#Q3a
 	band = signatures.map(lambda a:LSH(a[0], a[1]))
 	flip = band.flatMap(lambda a:[(x, a[0]+" : ") for x in a[1]])
 	similiar = flip.reduceByKey(lambda a,b:a+b)
 	similiar_combine = similiar.map(lambda a: similarity(a[0], a[1]))
 	similarImage = similiar_combine.reduceByKey(lambda a,b:a+b)
-	print_similarImage = similarImage.map(lambda a:printSimiliar(a[0], a[1]))
-	print_similarImage.collect()
+	print_similarImage = similarImage.map(lambda a:printSimiliar(a[0], a[1]))	#Q3b
+	candidateData = print_similarImage.collect()
+
+	for candi in candidateData:
+		if candi != None:
+			fileName, similiar_files = candi
+			print(fileName, similiar_files)
+	
+	dimensionReduction = print_feature.mapPartitions(dimenReduce)			#Q3c
+	svd = dimensionReduction.collectAsMap()
+	#print(svd)
+
+	##### Euclidean Distance between 3677454_2025195.zip-1 and 3677454_2025195.zip-18
+	feat1 = svd['3677454_2025195.zip-1']
+	feat2 = svd['3677454_2025195.zip-18']
+	sum = 0
+	for k in range(10):
+		sum += math.pow((feat1.item(k)-feat2.item(k)),2)
+	print("3677454_2025195.zip-1 -> 3677454_2025195.zip-18, Euc distance: ", math.sqrt(sum))
+
+	eucDist1 = []
+	eucDist2 = []
+	for data in candidateData:
+		if data != None and data[0] != '':
+			mainImage = data[0]
+			mainFeatures = svd[mainImage]
+			candidate_list = data[1]
+			for candidate in candidate_list:
+				features = svd[candidate]
+				sum = 0
+				for k in range(10):
+					sum += math.pow((mainFeatures.item(k)-features.item(k)),2)
+				if mainImage == '3677454_2025195.zip-1':
+					eucDist1.append((mainImage, candidate, math.sqrt(sum)))
+				elif mainImage == '3677454_2025195.zip-18':
+					eucDist2.append((mainImage, candidate, math.sqrt(sum)))
+#				print(mainImage,"->",candidate, "Euc distance: ", math.sqrt(sum))
+	eucDist1 = sorted(eucDist1, key=lambda a: a[2])
+	eucDist2 = sorted(eucDist2, key=lambda a: a[2])
+	for val in eucDist1:
+		print(val[0],"->",val[1], "Euc distance: ", val[2])
+
+	for val in eucDist2:
+		print(val[0],"->",val[1], "Euc distance: ", val[2])
 
 
 
